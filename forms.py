64fc66163b6b51e29fc8fbff3a14c7cf5dd2aa4b
carlosdfranca <carlosdudu258@gmail.com>
2022-04-28 14:04:05 -0300

    Form formatting
    Formatando o formulário da página de Login e Criar Conta

--- forms.py
+++ forms.py
@@ -1,15 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class FormCriarConta(FlaskForm):
     username = StringField('Nome de Usuário', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    confirmacao_senha = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('senha')])
+    confirm_senha = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
     
 class FormLogin(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    botao_submit_Login = SubmitField('Fazer Login')
+    lembrar_dados = BooleanField('Lembrar dados de acesso')
+    botao_submit_login = SubmitField('Fazer Login')
