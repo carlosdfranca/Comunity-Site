e182ea43bc5d1d60c46c4676dbb4061ac5c96168
carlosdfranca <carlosdudu258@gmail.com>
2022-04-28 14:09:50 -0300

    post authorization
    autorizei o método POST no meu site de login para conseguir receber as informações do formulário.

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     form_login = FormLogin()
     form_criar_conta = FormCriarConta()
